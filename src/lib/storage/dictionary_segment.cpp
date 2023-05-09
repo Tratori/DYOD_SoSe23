03db36a003549d409f2a754988e7cdc2b955207d
rothaarlappen <roessler.paul@web.de>
2023-05-09 10:51:26 +0200

    implements creation of dictionary segment and FixedWidthIntegerVector

--- src/lib/storage/dictionary_segment.cpp
+++ src/lib/storage/dictionary_segment.cpp
@@ -7,14 +7,26 @@ namespace opossum {
 
 template <typename T>
 DictionarySegment<T>::DictionarySegment(const std::shared_ptr<AbstractSegment>& abstract_segment) {
+  // TODO: Split up into 2 methods, creating the dictionary and the attribute vector each. 
+  
   // creating dictionary
   auto value_segment = std::dynamic_pointer_cast<ValueSegment<T>>(abstract_segment);
   std::vector<T> values = value_segment->values();
   std::sort(values.begin(), values.end());
   auto last_distinct = std::unique(values.begin(), values.end());
   values.erase(last_distinct, values.end());
-  _dictionary = values;
 
+  // If you have an idea how to avoid the following 7 lines, feel free
+  auto contains_null = value_segment->is_nullable() && (std::find(value_segment->null_values().begin(), value_segment->null_values().end(), true) != value_segment->null_values().end()); 
+  if(contains_null){
+    values.erase(values.begin());
+  }
+  if(values.size() + contains_null + 1 > std::numeric_limits<ChunkOffset>::max()){
+    Fail("Dictionary Segment cannot represent INVALID_VALUE_ID, NULL Values and all other values.");
+  }
+
+  _dictionary = values;
+  
   // creating lookup
   std::unordered_map<T, ValueID> inverted_dictionary;
   u_int32_t dict_size = _dictionary.size(); 
@@ -22,49 +34,63 @@ DictionarySegment<T>::DictionarySegment(const std::shared_ptr<AbstractSegment>& 
     inverted_dictionary.insert({_dictionary[dict_key], ValueID{dict_key}});
   }
 
+  auto vector_size = abstract_segment->size(); 
   // initialize attribute vector with size of current segment
-  _attribute_vector = std::make_shared<FixedWidthIntegerVector>(abstract_segment->size());
+  _attribute_vector = std::make_shared<FixedWidthIntegerVector>(vector_size);
+
   // filling attribute vector with index of values
-  int values_size = values.size(); 
-  for(int val_index = 0; val_index < values_size; val_index++){
-    _attribute_vector->set(val_index, inverted_dictionary[values[val_index]]);
+  for(ChunkOffset val_index = 0; val_index < vector_size; val_index++){
+    if(value_segment->null_values()[val_index]){
+      _attribute_vector->set(val_index, null_value_id());
+    } else {
+      _attribute_vector->set(val_index, inverted_dictionary[values[val_index]]);
+    }
   }
 }
 
 template <typename T>
 AllTypeVariant DictionarySegment<T>::operator[](const ChunkOffset chunk_offset) const {
   // Implementation goes here
-  Fail("Implementation is missing.");
+  // Fail("Implementation is missing.");
+  return _dictionary[_attribute_vector->get(chunk_offset)];
 }
 
 template <typename T>
 T DictionarySegment<T>::get(const ChunkOffset chunk_offset) const {
   // Implementation goes here
-  Fail("Implementation is missing.");
+  // Fail("Implementation is missing.");
+  return _dictionary[_attribute_vector->get(chunk_offset)];
 }
 
 template <typename T>
 std::optional<T> DictionarySegment<T>::get_typed_value(const ChunkOffset chunk_offset) const {
   // Implementation goes here
-  Fail("Implementation is missing.");
+  // Fail("Implementation is missing.");
+  if(_attribute_vector->get(chunk_offset) == null_value_id()){
+    return std::nullopt;
+  }
+  return _dictionary[_attribute_vector->get(chunk_offset)];
 }
 
 template <typename T>
 const std::vector<T>& DictionarySegment<T>::dictionary() const {
   // Implementation goes here
-  Fail("Implementation is missing.");
+  //Fail("Implementation is missing.");
+  return _dictionary; 
 }
 
 template <typename T>
 std::shared_ptr<const AbstractAttributeVector> DictionarySegment<T>::attribute_vector() const {
   // Implementation goes here
-  Fail("Implementation is missing.");
+  // Fail("Implementation is missing.");
+  return _attribute_vector; 
 }
 
 template <typename T>
 ValueID DictionarySegment<T>::null_value_id() const {
   // Implementation goes here
-  Fail("Implementation is missing.");
+  return static_cast<ValueID>(_dictionary.size()); 
+  // Fail("Implementation is missing.");
 }
 
 template <typename T>
@@ -100,13 +126,14 @@ ValueID DictionarySegment<T>::upper_bound(const AllTypeVariant& value) const {
 template <typename T>
 ChunkOffset DictionarySegment<T>::unique_values_count() const {
   // Implementation goes here
-  Fail("Implementation is missing.");
+  // Fail("Implementation is missing.");
+  return _dictionary.size(); 
 }
 
 template <typename T>
 ChunkOffset DictionarySegment<T>::size() const {
   // Implementation goes here
-  return ChunkOffset{};
+  return static_cast<ChunkOffset>(_attribute_vector->size());
 }
 
 template <typename T>
--- src/lib/storage/fixed_width_integer_vector.cpp
+++ src/lib/storage/fixed_width_integer_vector.cpp
@@ -2,25 +2,36 @@
 
 namespace opossum {
 
-FixedWidthIntegerVector::FixedWidthIntegerVector(size_t size) {}
+FixedWidthIntegerVector::FixedWidthIntegerVector(size_t size) {
+  // TODO: what should be our default value here? 
+  // do we want to store a Vector of ValueIDs instead of u_int32_t? 
+  _attribute_vector = std::vector<u_int32_t>(size, -1); 
+}
 
 ValueID FixedWidthIntegerVector::get(const size_t index) const {
-  return ValueID{0};
+  if(index >= _attribute_vector.size()){
+    throw std::logic_error("Index out of bounds"); 
+  }
+  return ValueID{_attribute_vector[index]};
 }
 
 // Sets the value id at a given position.
 void FixedWidthIntegerVector::set(const size_t index, const ValueID value_id) {
-  return;
+  // Do we really want this check here or would DebugAssert be enough? (Same for the getter)
+  if(index >= _attribute_vector.size()){
+    throw std::logic_error("Index out of bounds"); 
+  }
+  _attribute_vector[index] = value_id; 
 }
 
 // Returns the number of values.
 size_t FixedWidthIntegerVector::size() const {
-  return 0;
+  return _attribute_vector.size();
 }
 
 // Returns the width of biggest value id in bytes.
 AttributeVectorWidth FixedWidthIntegerVector::width() const {
-  return 0;
+  return AttributeVectorWidth{4};
 }
 
 }  // namespace opossum
