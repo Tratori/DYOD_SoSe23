8a6295955bd5ac6163c0c9604c3426c415441162
Fabian Mahling <FabianMahling@web.de>
2023-05-30 22:07:27 +0200

    implement get table operator

---
+++ src/lib/operators/get_table.cpp
@@ -0,0 +1,16 @@
+#include "get_table.hpp"
+#include "storage/storage_manager.hpp"
+
+namespace opossum {
+GetTable::GetTable(const std::string& name) : _table_name{name} {};
+
+const std::string& GetTable::table_name() const {
+  return _table_name;
+}
+
+std::shared_ptr<const Table> GetTable::_on_execute() {
+  StorageManager& storageManager = StorageManager::get();
+  Assert(storageManager.has_table(_table_name), "Table " + _table_name + " does not exist");
+  return storageManager.get_table(_table_name);
+}
+}  // namespace opossum
